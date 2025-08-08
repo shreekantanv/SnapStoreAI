a690f74b7dc6ba03c2a58828da99448fe7fb6a12
sh92 <shreekanta.nv@gmail.com>
2025-08-08 09:13:18 -0400

    added localization

---
+++ client/lib/l10n/app_localizations_es.dart
@@ -0,0 +1,46 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get signIn => 'Iniciar Sesión';
+
+  @override
+  String get signInWithGoogle => 'Iniciar sesión con Google';
+
+  @override
+  String get continueAsGuest => 'Continuar como invitado';
+
+  @override
+  String get toolStore => 'Tienda de Herramientas';
+
+  @override
+  String get searchForTools => 'Buscar herramientas';
+
+  @override
+  String get all => 'Todos';
+
+  @override
+  String get tools => 'Herramientas';
+
+  @override
+  String get favorites => 'Favoritos';
+
+  @override
+  String get history => 'Historial';
+
+  @override
+  String get wallet => 'Billetera';
+
+  @override
+  String get settings => 'Ajustes';
+
+  @override
+  String get aiToolbox => 'Caja de Herramientas de IA';
+}
