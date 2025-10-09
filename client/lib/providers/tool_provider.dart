8f5712d3b07070ad9a30c6875984a79a4ca478b3
shreekantanv <shreekanta.nv@gmail.com>
2025-10-08 21:29:42 -0400

    Cache tool stream to prevent UI flicker

--- client/lib/providers/tool_provider.dart
+++ client/lib/providers/tool_provider.dart
@@ -4,15 +4,16 @@ import '../models/tool.dart';
 
 class ToolProvider {
   final FirebaseFirestore _firestore;
+  late final Stream<List<Tool>> _toolsStream;
 
   ToolProvider({FirebaseFirestore? firestore})
-      : _firestore = firestore ?? FirebaseFirestore.instance;
-
-  Stream<List<Tool>> get tools {
-    return _firestore.collection('tools').snapshots().map((snap) {
+      : _firestore = firestore ?? FirebaseFirestore.instance {
+    _toolsStream = _firestore.collection('tools').snapshots().map((snap) {
       return snap.docs.map((d) => Tool.fromJson(d.data(), d.id)).toList();
     });
   }
+
+  Stream<List<Tool>> get tools => _toolsStream;
 
   Stream<List<Tool>> getToolsByIds(List<String> ids) {
     if (ids.isEmpty) {
